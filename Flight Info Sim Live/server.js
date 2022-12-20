5f4f2e10a2c33457b4ea4f124f35aa66b59bff08
Brandon Stinson <barnden@berkeley.edu>
2022-12-20 12:44:16 -0800

    Basic server file, added .env to hide API key

--- Flight Info Sim Live/server.js
+++ Flight Info Sim Live/server.js
@@ -1,2 +1,29 @@
 const PORT = 8000;
+const express = require('express');
+const axios = require("axios");
+const cors = require('cors');
+const dotenv =  require('dotenv');
 
+const app = express()
+dotenv.config();
+app.use(cors());
+
+app.get('/flights', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://toronto-pearson-airport.p.rapidapi.com/departures',
+        headers: {
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+          'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          res.json();
+      }).catch(function (error) {
+          console.error(error);
+      });
+});
+
+app.listen(PORT, () => console.log('running on PORT ' + PORT));
